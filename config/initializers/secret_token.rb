bd5ece3ebb68ecf5508be6a2669ba5d37f37bc78
Kenneth Vedder <kenneth.vedder@gmail.com>
2014-02-01 10:19:52 -0500

    this is the first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ptinteresting::Application.config.secret_key_base = 'c1e71888a4d2b293b5d427921a98a58ba10e3cfad3f0eeb993edc23071138e408063187fd957c37df2b71ef5bb9133d41507e5988f64e97f7b00f5bdb525aa46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ptinteresting::Application.config.session_store :cookie_store, key: '_ptinteresting_session'
